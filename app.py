09ac2f8230ddc5e82d391d5cd855fe2932808ebb
rjgplbvgh <75480701+rjgplbvgh@users.noreply.github.com>
2025-05-08 16:40:56 +0200

    Update app.py
    Added /read_file

--- app.py
+++ app.py
@@ -28,7 +28,12 @@ COLOR = random.choice(["red", "green", "blue", "blue2", "darkblue", "pink"])
 def main():
     # return 'Hello'
     return render_template('hello.html', name=socket.gethostname(), color=color_codes[COLOR])
-
+    
+@app.route('/read_file')
+def read_file():
+    f=open("/data/testfile.txt")
+    contents=f.read()
+    return render_template('hello.html', name=socket.gethostname(), color=color_codes[COLOR])
 
 if __name__ == "__main__":
 
